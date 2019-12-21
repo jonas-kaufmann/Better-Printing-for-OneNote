0e7ea1ccb6f7f8f461c29a883775c803f5702ce0
Fabian Thomas <fabithomi@web.de>
2019-12-21 23:33:45 +0100

    implemented Splitting of PS files to FixedDocuments

--- Better-Printing-for-OneNote/MainWindow.xaml.cs
+++ Better-Printing-for-OneNote/MainWindow.xaml.cs
@@ -1,20 +1,6 @@
-﻿using Better_Printing_for_OneNote.AdditionalClasses;
-using Better_Printing_for_OneNote.ViewModels;
+﻿using Better_Printing_for_OneNote.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Better_Printing_for_OneNote
 {
@@ -23,41 +9,8 @@ namespace Better_Printing_for_OneNote
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool DEBUG_MODE = false;
-        private FileStream _loggingFileStream;
-        private StreamWriter _loggingWriter;
-
         public MainWindow(string argFilePath)
         {
-            // Logging
-#if DEBUG
-            DEBUG_MODE = true;
-#endif
-            if (!DEBUG_MODE)
-            {
-                var path = Environment.GetFolderPath(
-                        Environment.SpecialFolder.LocalApplicationData) + "\\" + GeneralHelperClass.FindResource("LocalFolderTitle");
-                try
-                {
-                    Directory.CreateDirectory(path);
-                    _loggingFileStream = new FileStream(path + "\\log.txt",
-                        FileMode.Create, FileAccess.Write);
-                    _loggingWriter = new StreamWriter(_loggingFileStream);
-                    _loggingWriter.AutoFlush = true;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine("Logging konnte nicht aktiviert werden.");
-                    Console.WriteLine(e.Message);
-                    File.Delete(path + "\\log.txt");
-                }
-                Console.SetOut(_loggingWriter);
-                Console.SetError(_loggingWriter);
-                Console.WriteLine("Release-Mode");
-            }
-            else Console.WriteLine("Debug-Mode");
-
-            //ViewModel
             var viewModel = new MainWindowViewModel(argFilePath);
             DataContext = viewModel;
             viewModel.BringWindowToFrontEvent += new EventHandler(BringWindowToFront);
@@ -65,22 +18,23 @@ namespace Better_Printing_for_OneNote
             InitializeComponent();
         }
 
+
         private void BringWindowToFront(object sender, EventArgs e)
         {
             Dispatcher.Invoke(() =>
             {
-                if (!this.IsVisible)
+                if (!IsVisible)
                 {
-                    this.Show();
+                    Show();
                 }
-                if (this.WindowState == WindowState.Minimized)
+                if (WindowState == WindowState.Minimized)
                 {
-                    this.WindowState = WindowState.Normal;
+                    WindowState = WindowState.Normal;
                 }
-                this.Activate();
-                this.Topmost = true;
-                this.Topmost = false;
-                this.Focus();
+                Activate();
+                Topmost = true;
+                Topmost = false;
+                Focus();
             });
         }
     }
--- Better-Printing-for-OneNote/ViewModels/MainWindowViewModel.cs
+++ Better-Printing-for-OneNote/ViewModels/MainWindowViewModel.cs
@@ -3,6 +3,17 @@ using System.Windows;
 using Better_Printing_for_OneNote.AdditionalClasses;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Windows.Xps.Packaging;
+using System.IO;
+using Ghostscript.NET.Processor;
+using System.Collections.Generic;
+using Ghostscript.NET;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Documents;
+using System.Windows.Controls;
+using System.Diagnostics;
+using System.Drawing;
 
 namespace Better_Printing_for_OneNote.ViewModels
 {
@@ -12,6 +23,11 @@ namespace Better_Printing_for_OneNote.ViewModels
         #region Properties
 
         public event EventHandler BringWindowToFrontEvent;
+
+        private string LOCAL_FOLDER
+        {
+            get => Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\" + GeneralHelperClass.FindResource("LocalFolderTitle");
+        }
 
         private string TITLE
         {
@@ -36,8 +52,45 @@ namespace Better_Printing_for_OneNote.ViewModels
             }
             set
             {
-                _filePath = value;
-                OnPropertyChanged("FilePath");
+                // split document
+                if (File.Exists(value))
+                {
+                    _filePath = value;
+                    OnPropertyChanged("FilePath");
+                    Document = PngToFixedDoc(PsToPng(value), Signature);
+                }
+                else
+                {
+                    MessageBox.Show("Die zu öffnende Postscript Datei existiert nicht oder ist nicht valide. Bitte mit valider Datei neu versuchen.");
+                }
+            }
+        }
+
+        private string _signature;
+        public string Signature
+        {
+            get
+            {
+                return _signature;
+            }
+            set
+            {
+                _signature = value;
+                ChangeSignature(Document, value);
+            }
+        }
+
+        private FixedDocument _document;
+        public FixedDocument Document
+        {
+            get
+            {
+                return _document;
+            }
+            set
+            {
+                _document = value;
+                OnPropertyChanged("Document");
             }
         }
 
@@ -45,10 +98,11 @@ namespace Better_Printing_for_OneNote.ViewModels
 
         public MainWindowViewModel(string argFilePath)
         {
-            FilePath = argFilePath;
+            if (argFilePath != "")
+                FilePath = argFilePath;
 
             //eventuell unnötig
-            Task.Run(() => 
+            Task.Run(() =>
             {
                 Thread.Sleep(2000);
                 BringWindowToFrontEvent?.Invoke(this, EventArgs.Empty);
@@ -57,7 +111,142 @@ namespace Better_Printing_for_OneNote.ViewModels
 
         private void Test()
         {
-            MessageBox.Show(FilePath);
+        }
+
+        /// <summary>
+        /// Changes the signature of all Sites without recreating the document
+        /// </summary>
+        /// <param name="document">modified Fixeddocument</param>
+        /// <param name="signature">signature to set</param>
+        private void ChangeSignature(FixedDocument document, string signature)
+        {
+            // Signatur anpassen
+            foreach (var page in Document.Pages)
+                ((page.Child.Children[0] as StackPanel).Children[0] as TextBlock).Text = signature;
+        }
+
+        /// <summary>
+        /// Converts png to a FixedDocument
+        /// </summary>
+        /// <param name="imagePath">path to the image to convert</param>
+        /// <param name="documentWidth">width of the output document; default: DINA4</param>
+        /// <param name="documentHeight">height of the output document; default: DINA4</param>
+        /// <param name="signature">signature on every site of the document</param>
+        /// <param name="cropHeight">height to split</param>
+        /// <param name="srcBitmap">just ignore (its for the helper Method with "int cropHeight")</param>
+        /// <returns>the document</returns>
+        private FixedDocument PngToFixedDoc(string imagePath, string signature, int[] cropHeights, double documentWidth = 793.7007874, double documentHeight = 1122.519685, Bitmap src = null)
+        {
+            FixedDocument document = new FixedDocument();
+            Bitmap srcBitmap = src ?? System.Drawing.Image.FromFile(imagePath) as Bitmap;
+            var cropPostionY = 0;
+            foreach (var cropHeight in cropHeights)
+            {
+                // Crop Image
+                Rectangle cropRect;
+                cropRect = new Rectangle(0, cropPostionY, srcBitmap.Width, cropHeight);
+                cropPostionY += cropHeight;
+                var targetBitmap = new Bitmap(cropRect.Width, cropRect.Height);
+                using (Graphics g = Graphics.FromImage(targetBitmap))
+                {
+                    g.DrawImage(srcBitmap, new Rectangle(0, 0, targetBitmap.Width, targetBitmap.Height), cropRect, GraphicsUnit.Document);
+                }
+
+                // Convert Bitmap to BitmapImage
+                BitmapImage croppedImage = new BitmapImage();
+                MemoryStream ms = new MemoryStream();
+                targetBitmap.Save(ms, srcBitmap.RawFormat);
+                croppedImage.BeginInit();
+                ms.Seek(0, SeekOrigin.Begin);
+                croppedImage.StreamSource = ms;
+                croppedImage.EndInit();
+
+                // Create page and add it to the FixedDocument
+                var page = new PageContent();
+                var fixedPage = new FixedPage();
+                fixedPage.Width = documentWidth;
+                fixedPage.Height = documentHeight;
+                page.Child = fixedPage;
+                var stackpanel = new StackPanel();
+                var imageControl = new System.Windows.Controls.Image() { Source = croppedImage };
+                imageControl.Width = documentWidth;
+                var signatureTB = new TextBlock() { Text = signature };
+                stackpanel.Children.Add(signatureTB);
+                stackpanel.Children.Add(imageControl);
+                page.Child.Children.Add(stackpanel);
+                document.Pages.Add(page);
+            }
+
+            return document;
+        }
+
+        /// <summary>
+        /// Converts png to a FixedDocument
+        /// </summary>
+        /// <param name="imagePath">path to the image to convert</param>
+        /// <param name="documentWidth">width of the output document; default: DINA4</param>
+        /// <param name="documentHeight">height of the output document; default: DINA4</param>
+        /// <param name="signature">signature on every site of the document</param>
+        /// <param name="cropHeight">height to split</param>
+        /// <returns>the document</returns>
+        private FixedDocument PngToFixedDoc(string imagePath, string signature, int cropHeight = 3500, double documentWidth = 793.7007874, double documentHeight = 1122.519685)
+        {
+            var srcBitmap = System.Drawing.Image.FromFile(imagePath) as Bitmap;
+            var splits = (int)Math.Ceiling(decimal.Divide(srcBitmap.Height, cropHeight));
+            var cropHeights = new int[splits];
+            for (int i = 0; i < splits - 1; i++)
+                cropHeights[i] = cropHeight;
+            cropHeights[splits - 1] = srcBitmap.Height - (splits - 1) * cropHeight;
+            return PngToFixedDoc(imagePath, signature, cropHeights, documentWidth, documentHeight, srcBitmap);
+        }
+
+        /// <summary>
+        /// Converts Ps file to a Png file and stores it in the Temp Directory (if the conversion fails the program exits with error messages)
+        /// </summary>
+        /// <param name="filePath">Path to the PS file</param>
+        /// <returns>Path to converted file</returns>
+        private string PsToPng(string filePath)
+        {
+            if (GhostscriptVersionInfo.IsGhostscriptInstalled)
+            {
+                var temp = LOCAL_FOLDER + "\\Temp";
+                FileInfo fileInfo = new FileInfo(filePath);
+                var outputPath = Path.Combine(temp,
+                    string.Format("{0}.png", Path.GetFileNameWithoutExtension(fileInfo.Name)));
+
+                try
+                {
+                    using (GhostscriptProcessor processor = new GhostscriptProcessor())
+                    {
+                        List<string> switches = new List<string>();
+                        switches.Add("empty");
+                        switches.Add("-dSAFER");
+                        switches.Add("-dBATCH");
+                        switches.Add("-r300");
+                        switches.Add("-sDEVICE=png16m");
+                        switches.Add("-sOutputFile=" + outputPath);
+                        switches.Add(filePath);
+
+                        GeneralHelperClass.CreateDirectoryIfNotExists(temp);
+                        processor.StartProcessing(switches.ToArray(), null);
+
+                        return outputPath;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Es kam zu einem Fehler. Mehr Informationen sind in den Log-Dateien in { LOCAL_FOLDER } hinterlegt.");
+                    Console.WriteLine($"Application Shutdown: PsToPng conversion failed:\n {ex.ToString()}");
+                    Application.Current.Shutdown();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Bitte installieren Sie Ghostscript. (Es wird die 64-bit Version benötigt)");
+                Console.WriteLine("Application Shutdown: Ghostscript is not installed (64-bit needed)");
+                Application.Current.Shutdown();
+            }
+            return "";
         }
     }
 }
