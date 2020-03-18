6211fc09b1853d0f444d0b96d94de82dee2c941c
Fabian Thomas <fabithomi@web.de>
2020-03-18 12:17:18 +0100

    added preset model

---
+++ Better-Printing-for-OneNote/Models/PresetModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Better_Printing_for_OneNote.Models
+{
+    class PresetModel
+    {
+        public string Name { get; set; }
+    }
+}
